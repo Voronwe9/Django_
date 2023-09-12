2cee1c45d9773d9f0b565076a68dbcb1bf029310
Barynin Anton <antonbarynin@yandex.ru>
2023-09-12 21:37:18 +0300

    HW 2.1

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,6 +1,8 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import slugify
+
 from phones.models import Phone
 
 
@@ -14,4 +16,13 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            new_phone = Phone(
+                id=int(phone['id']),
+                name=phone['name'],
+                image=phone['image'],
+                price=phone['price'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name']))
+            new_phone.save()
+        return print('Status: OK')
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,22 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sort_pages = request.GET.get('sort')
+    phone_objects = Phone.objects.all()
+    if sort_pages == 'max_price':
+        phone_objects = phone_objects.order_by('price').reverse()
+    elif sort_pages == 'min_price':
+        phone_objects = phone_objects.order_by('price')
+    elif sort_pages == 'name':
+        phone_objects = phone_objects.order_by('name')
+    context = {'phones': phone_objects}
+    return render(request, template, context=context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
+    return render(request, template, context=context)
+
+
