c52a3aceb086edc9bb53d68d743b0b3487a2df0d
Barynin Anton <antonbarynin@yandex.ru>
2023-10-07 16:09:55 +0300

    change

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Section, ArticleSection
+
+
+class ArticleSectionInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        flag = 0
+        for form in self.forms:
+            if form.cleaned_data.get('DELETE'):
+                continue
+            if form.cleaned_data.get('main_section'):
+                flag += 1
+            if flag > 1:
+                raise ValidationError('Выберите 1 основной раздел.')
+        if flag == 0:
+            raise ValidationError('Выберите 1 основной раздел.')
+        return super().clean()
+
+
+class ArticleSectionInline(admin.TabularInline):
+    model = ArticleSection
+    extra = 7
+    formset = ArticleSectionInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at', 'image', ]
+    list_filter = ['published_at', ]
+    inlines = [ArticleSectionInline, ]
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -2,15 +2,37 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение', )
+    sections = models.ManyToManyField('Section', related_name='articles', through='ArticleSection')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
 
     def __str__(self):
         return self.title
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Тематика')
+
+    class Meta:
+        verbose_name = 'Тематика'
+        verbose_name_plural = 'Тематика'
+
+    def __str__(self):
+        return self.name
+
+
+class ArticleSection(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    tag = models.ForeignKey(Section, on_delete=models.CASCADE, related_name='scopes', verbose_name='Тематика')
+    main_section = models.BooleanField(verbose_name='Основной раздел')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статей'
